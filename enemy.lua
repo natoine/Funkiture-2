ee76ff5e67a72cd4fbb6867c9d1b11e795152e06
natoine <natoine@natoine-laptop.(none)>
2013-07-21 16:05:00 +0200

    now they hit

--- enemy.lua
+++ enemy.lua
@@ -100,8 +100,10 @@ function enemy_mt:update(dt)
 	--tapera ou tapera pas
 	if self.currentcycle == enemy.cycles.walk or self.currentcycle == enemy.cycles.idle then
 		--print("distance :"..nearestPlayerInfo[2])
-		if nearestPlayerInfo[2] < distanceBtwEnemies then
-			if ( nearestPlayerInfo[1] == 1 and self.left) or (nearestPlayerInfo[1] == -1 and not self.left) then
+		if nearestPlayerInfo[2] <= distanceBtwEnemies then
+			--print("distance hit")
+			if ( nearestPlayerInfo[1] == -1 and self.left) or (nearestPlayerInfo[1] == 1 and not self.left) then
+				--print("direction hit")
 				local testKick = math.random(1)
 				if testKick > 0.5 then 
 					self.currentcycle = enemy.cycles.kick
@@ -139,33 +141,33 @@ function enemy_mt:update(dt)
 		end
 		
 	--deplacement
-	local lastX = self.x
-	if self.x >= love.graphics.getWidth() then
-		self.left = true
-	elseif self.x <= 0 then
-		self.left = false
-	end
-	if nearestPlayerInfo[2] > distanceBtwEnemies then
-		local intensity = self:getDirection()
-		local xintensity = intensity[1]	
-		self.x = self.x + self.speed * xintensity * dt
-		if xintensity < 0 then
+		local lastX = self.x
+		if self.x >= love.graphics.getWidth() then
 			self.left = true
-		else 
+		elseif self.x <= 0 then
 			self.left = false
 		end
-	end
-	--print(lastX - self.x)
-	--0.03 seuil empririque TODO passer en variable 
-	if self.currentcycle == enemy.cycles.idle and ( lastX - self.x > 0.03 or lastX - self.x < -0.03) then
-		self.currentcycle = enemy.cycles.walk
-		self.curframe = 1
- 	elseif self.currentcycle == enemy.cycles.walk then
-		if lastX - self.x < 0.03 and lastX - self.x > -0.03 then
-			self.currentcycle = enemy.cycles.idle
-			self.curframe = 1
+		if nearestPlayerInfo[2] > distanceBtwEnemies then
+			local intensity = self:getDirection()
+			local xintensity = intensity[1]	
+			self.x = self.x + self.speed * xintensity * dt
+			if xintensity < 0 then
+				self.left = true
+			else 
+				self.left = false
+			end
 		end
-	end
+		--print(lastX - self.x)
+		--0.03 seuil empririque TODO passer en variable 
+		if self.currentcycle == enemy.cycles.idle and ( lastX - self.x > 0.03 or lastX - self.x < -0.03) then
+			self.currentcycle = enemy.cycles.walk
+			self.curframe = 1
+	 	elseif self.currentcycle == enemy.cycles.walk then
+			if lastX - self.x < 0.03 and lastX - self.x > -0.03 then
+				self.currentcycle = enemy.cycles.idle
+				self.curframe = 1
+			end
+		end
 end
 
 -- Retourne un tableau : en première case (x) et en deuxième case (y)
