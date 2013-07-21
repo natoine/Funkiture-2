da2687b22c5a1b0deb8ff5ca21fc341893dbb811
Anthony <anthony@naturalpad.fr>
2013-07-21 12:20:43 +0200

    Adding new music

--- main.lua
+++ main.lua
@@ -30,7 +30,8 @@ function love.load()
 	musics[2] = love.audio.newSource("resources/Sounds/music2.mp3", "stream")
 	musics[3] = love.audio.newSource("resources/Sounds/music3.mp3", "stream")
 	musics[4] = love.audio.newSource("resources/Sounds/music4.mp3", "stream")
-	currentMusicIndex = math.round(math.random(1, 4))
+	musics[5] = love.audio.newSource("resources/Sounds/music5.mp3", "stream")
+	currentMusicIndex = math.round(math.random(1, #musics))
 	
 	love.audio.play(musics[currentMusicIndex])
 end
@@ -47,7 +48,7 @@ function love.update(dt)
 	gstate.update(dt)
 	if(musics[currentMusicIndex]:isStopped()) then
 		musics[currentMusicIndex]:rewind()
-		currentMusicIndex = math.round(math.random(1, 4))
+		currentMusicIndex = math.round(math.random(1, #musics))
 		love.audio.play(musics[currentMusicIndex])
 	end
 end
