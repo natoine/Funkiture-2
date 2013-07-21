6ea69b3aec2823669ca5a06a9eb6005b0358fe49
Bressand <j.bressand@hotmail.fr>
2013-07-21 18:46:12 +0200

    pumpelump here we go !

--- hud.lua
+++ hud.lua
@@ -1,6 +1,7 @@
 local hud_mt = {}
 local hud = {}
 imagelife = love.graphics.newImage("resources/textures/hud/life.png")
+quad3 = love.graphics.newQuad(11, 12, 50, 37, 1024,128)
 --quad = {}
 --table.insert(quad, love.graphics.newQuad(0,0,128,32,128,64))
 --table.insert(quad, love.graphics.newQuad(0,32,128,32,128,64))--
@@ -23,8 +24,7 @@ function hud_mt:drawplayer(x,y)
 	love.graphics.setColor(255,255,255,200)
 	love.graphics.drawq(imagelife, quad1,x + 10 + imagelife:getWidth()/2,y + imagelife:getHeight()/4,0,1,1,imagelife:getWidth()/2,imagelife:getHeight()/4)
 	if self.player.life > 0 then
-		love.graphics.print("je suis vivant! "..self.player.life,500,500)
-		quad2 = love.graphics.newQuad(0,0,128*self.player.life/100,32,128,64)
+		local quad2 = love.graphics.newQuad(0,0,128*self.player.life/100,32,128,64)
 		love.graphics.drawq(imagelife, quad2,x+ 10 + imagelife:getWidth()/2,y + imagelife:getHeight()/4,0,1,1,imagelife:getWidth()/2,imagelife:getHeight()/4)
 	end
 	love.graphics.setColor(0,0,0,200)
@@ -36,8 +36,9 @@ function hud_mt : drawenemy (y)
 	love.graphics.setColor(255,255,255,200)
 	love.graphics.drawq(imagelife, quad1,800 + imagelife:getWidth()/2,y + 100 + imagelife:getHeight()/2,0,1,1,imagelife:getWidth()/2,imagelife:getHeight()/4)
 	if self.enemy.life > 0 then
-		quad2 = love.graphics.newQuad(0,0,128*self.enemy.life/100,32,128,64)
-		love.graphics.drawq(imagelife, quad2,800+ imagelife:getWidth()/2,y + 100 + imagelife:getHeight()/2,0,1,1,imagelife:getWidth()/2,imagelife:getHeight()/4)
+		local quad2 = love.graphics.newQuad(0,0,128*self.enemy.life/100,32,128,64)
+		love.graphics.drawq(imagelife, quad2,800+ imagelife:getWidth()/2, y + 100 + imagelife:getHeight()/2,0,1,1,imagelife:getWidth()/2,imagelife:getHeight()/4)
+		love.graphics.drawq(self.enemy.image, quad3, 800 - 24, y + 100 + imagelife:getHeight()/2,0,1,1, 36, 30 )
 	end
 end
 
